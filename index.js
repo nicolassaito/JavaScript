9d351de88fc6c0c4f077172970920d8428a37633
Nicolas.Saito <nicolaslsaito321@gmail.com>
2024-04-01 10:28:19 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UFT-8">
+    <title>Javascript Hello World!</title>    
+    
+    <script>
+function main(){
+    alert("Hello World!");
+}
+</script>
+</head>
+<body>
+
+</body>
+</html>
